d58727037294eb1a0aa3c9a032823849a6bba5c1
Vijay Misal <misalvijay153@gmail.com>
2023-10-24 10:41:24 +0530

    final commit

--- Node JS Sheriyans/script3.js
+++ Node JS Sheriyans/script3.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const app = express()
 
-app.use(function (req, res, next) {
-    console.log("Middleware working")
-    next()
-});
 
+app.set('view engine', 'ejs')
+ //middleware
+// app.use(function (req, res, next) {
+//     console.log("Middleware working")
+//     next()
+// });
+
+//rendering index.ejs file
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('index')
 })
 
-app.get('/profile', function (req, res) {
-    res.send("Hello profile")
+// //rendering contact.ejs file     
+// app.get('/contact', function (req, res) {
+//     res.render('contact')
+// })
+
+//static route
+// app.get('/profile', function (req, res) {
+//     res.send("Hello profile")
+// })
+
+// //dynamic route
+// app.get("/profile/:username", function (req, res) {
+//     res.send(`Hello ${req.params.username}`)
+// })
+
+app.get("/profile", function (req, res) {
+    res.render('profile')
+
 })
+
 app.listen(3000)
